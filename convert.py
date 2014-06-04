76311b22f7c73ab7bb759c7ddc847268cd0aed8b
Vikram Shirgur <vikram@shirgur.org>
2014-06-04 14:04:16 -0700

    Almost got things working, need to add support for multiple img tags.

--- convert.py
+++ convert.py
@@ -3,15 +3,17 @@
 Converts markdown files written for octopress in markdown format to Restructured Text format suitable for use in nikola.
 """
 
+import os
 import sys
 import glob
+import re
 from subprocess import check_output
 from datetime import datetime
 
 
-PANDOC_TOOL = '/usr/bin/pandoc'
+PANDOC_TOOL = '/usr/bin/pandoc' # Tested with 1.12.2.1
 
-def get_pandoc_output(octo_file):
+def get_rst(octo_file):
     args = []
     args.append(PANDOC_TOOL)
     args.append('-f')
@@ -20,13 +22,26 @@ def get_pandoc_output(octo_file):
     args.append('rst')
     args.append(octo_file)
     output = check_output(args)
-    return output
+    return output.split('\n')
+
+def cleanup_rst(rst):
+    """Remove octopress extensions to markdown. 
+    Matches: {% img /path/to/image %}
+    """
+    lines = []
+    for line in rst:
+#        m = re.match(r'.*?\{\%\s+img\s+([a-zA-Z0-9\\_\/.]+)\s+(.*?)\%\}', line)
+#        if m:
+#        img_path = m.group(1).replace('\\', '')
+        lines.append(re.sub(r'.*?\{\%\s+img\s+([a-zA-Z0-9\\_\/.]+)\s+(.*?)\%\}', r'\n.. figure:: \1\n:alt: \2\n', line))
+            
+    return lines
 
 def get_meta(octo_file):
     meta = {}
-    old_meta = open(octo_file).read().split('---\n')[1]
+    octo_meta = open(octo_file).read().split('---\n')[1]
     
-    for m in old_meta.split('\n'):
+    for m in octo_meta.split('\n'):
         if 'title' in m:
             k,v = m.split(':')
             meta['title'] = v.strip()
@@ -39,17 +54,43 @@ def get_meta(octo_file):
             k,v = m.split(':')
             v = v.strip().strip('[]')
             meta['tags'] = v
+
     return meta
+
+def get_rst_file(octo_file):
+    """Converts octopress file name (YYYY-MM-DD-slug.markdown) into the rst file (slug.rst)
+    """
+    d, l = os.path.split(octo_file)
+    tmp = octo_file.split('.markdown')[0]
+    rst_file = '-'.join(tmp.split('-')[3:])
+    rst_file += '.rst'
+    return os.path.join(d, rst_file)
 
 def main():
     octo_dir = sys.argv[1]
     niko_dir = sys.argv[2]
 
     for octo_file in glob.glob('%s/*.markdown' % octo_dir):
+        lines = []
         meta = get_meta(octo_file)
-        rst = get_pandoc_output(octo_file)
-        print meta
+        lines.append('.. title: %s' % meta.get('title', ''))
+        lines.append('.. slug: %s' % meta.get('slug', ''))
+        lines.append('.. date: %s' % meta.get('date', ''))
+        lines.append('.. tags: %s' % meta.get('tags', ''))
+        lines.append('.. link: %s' % meta.get('link', ''))
+        lines.append('.. description: %s' % meta.get('description', ''))
+        lines.append('.. type: %s' % meta.get('type', 'text'))
+        lines.append('')
+        rst_file = get_rst_file(octo_file)
+        f = open(rst_file, 'w')
+        f.write('\n'.join(lines))
 
+        rst = get_rst(octo_file)
+        f.write('\n'.join(cleanup_rst(rst)))
+        f.close()
+        print 'INFO: Processed (%s) -- (%s)' % (octo_file, rst_file)
+        
+    return
 
 
 if __name__ == '__main__':
